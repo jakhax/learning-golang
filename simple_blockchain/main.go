8ac9443645a0acb560ed12ca8dcec89674c51e21
jack ogina <jackogina60@gmail.com>
2018-08-14 07:33:12 +0300

    heart rate struct, go routine to create genesisblock

--- simple_blockchain/main.go
+++ simple_blockchain/main.go
@@ -3,24 +3,37 @@ package main
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"log"
 	"strconv"
+	"sync"
 	"time"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/joho/godotenv"
 )
 
 // block - items in a blockchain
 type Block struct {
-	Index     int    //position of data record on blockchain
-	Timestamp string // auto time of when data is added to the blockchain
-	BPM       int    // beats per minute - heartrate
-	Hash      string //SHA256 identifier of this data record
-	PrevHash  string //SHA256 identifier of the previous data on the block chain
+	Index     int    `json:"index"`     //position of data record on blockchain
+	Timestamp string `json:"timestamp"` // auto time of when data is added to the blockchain
+	BPM       int    `json:"bpm"`       // beats per minute - heartrate
+	Hash      string `json:"hash"`      //SHA256 identifier of this data record
+	PrevHash  string `json:"prevHash"`  //SHA256 identifier of the previous data on the block chain
+}
+
+type Heartrate struct {
+	BPM int
 }
 type Blockchain []Block
 
-func calculateHash(b Block) {
+var mutex = &sync.Mutex{}
+
+var blockchain Blockchain
+
+func calculateHash(b Block) string {
 	record := strconv.Itoa(b.Index) + b.Timestamp + strconv.Itoa(b.BPM) + b.PrevHash
 	h := sha256.New()
-	h.write([]byte(record))
+	h.Write([]byte(record))
 	return hex.EncodeToString(h.Sum(nil))
 }
 
@@ -29,13 +42,54 @@ func generateBlock(oldBlock Block, BPM int) (Block, error) {
 	var newBlock Block
 	t := time.Now()
 	newBlock.Index = oldBlock.Index + 1
-	newBlock.Timestamp = t.string()
+	newBlock.Timestamp = t.String()
 	newBlock.BPM = BPM
 	newBlock.PrevHash = oldBlock.Hash
 	newBlock.Hash = calculateHash(newBlock)
 	return newBlock, nil
 }
 
+// block validation
+func isBlockvalid(newBlock, oldBlock Block) bool {
+	if newBlock.Index != oldBlock.Index+1 {
+		return false
+	}
+	if oldBlock.Hash != newBlock.PrevHash {
+		return false
+	}
+	if calculateHash(newBlock) != newBlock.Hash {
+		return false
+	}
+	return true
+}
+
+//classical blockchain problem
+/* if we receive blockchains from 2 nodes from our block chain network which do we take ?
+sol: the longest - its up to date..assuming
+*/
+func replaceChain(newBlocks []Block) {
+	if len(blockchain) < len(newBlocks) {
+		blockchain = newBlocks
+	}
+	return
+}
+
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error ", err)
+	}
+	//create a go routine to create a genesis block
+	go func() {
+		t := time.Now()
+		//genesisBlock := Block{}
+		genesisBlock := Block{Index: 0, Timestamp: t.String(), BPM: 0, PrevHash: ""}
+		genesisBlock.Hash = calculateHash(genesisBlock)
+		mutex.Lock()
+		blockchain = append(blockchain, genesisBlock)
+		mutex.Unlock()
+		spew.Dump(blockchain)
+	}()
+	log.Fatal(runServer())
 
 }
